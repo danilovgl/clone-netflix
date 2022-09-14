1c67fb2e1c25214278af1523408122b36d18fe87
Danilo Vieira <nilovieira64@gmail.com>
2022-09-14 13:51:09 -0300

    adding footer in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import "./App.css";
 import { Row } from "./components/Row";
 import categories from "./api";
 import { Banner } from "./components/Banner";
 import { Navbar } from "./components/Navbar";
+import { FcAbout } from "react-icons/fc";
+
+
+import "./App.css";
 function App() {
   return (
     <div className="App">
@@ -11,6 +14,9 @@ function App() {
       {categories.map((e) => (
         <Row key={e.name} title={e.title} path={e.path} isLarge={e.isLarge} />
       ))}
+      <footer> 
+      <a href="https://www.linkedin.com/in/danilo-vieira-10515123a/"><div className="container-footer"> Desenvolvido por Danilo Vieira  <FcAbout size={25}/>  </div></a>
+      </footer>
     </div>
   );
 }
